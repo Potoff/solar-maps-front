83993e03fc444935711cf9fbac693518ce0ec9aa
Potoff <services.ankaamusic@gmail.com>
2023-11-08 16:47:58 +0100

    install tailwind, leaflet and initialize map

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,3 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,26 @@
 <script setup>
+import leaflet from 'leaflet';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  let map = leaflet.map('map').setView([45.766667, 4.834395], 8);
+  leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_APIKEY}`, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v12',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: import.meta.env.VUE_APP_APIKEY
+  }).addTo(map);
+  console.log(import.meta.env.VITE_APIKEY)
+});
+
 </script>
 
 <template>
-
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]">
+    </div>
+  </div>
 </template>
